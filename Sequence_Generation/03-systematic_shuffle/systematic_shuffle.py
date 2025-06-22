0bffe2564cd20086aad5c8f25f89bb4b06267037
hgao12345 <123959250+hgao12345@users.noreply.github.com>
2025-06-22 22:22:11 +0800

    Update systematic_shuffle.py

--- Sequence_Generation/03-systematic_shuffle/systematic_shuffle.py
+++ Sequence_Generation/03-systematic_shuffle/systematic_shuffle.py
@@ -1,17 +1,22 @@
 import itertools
 import csv
 import argparse
+import os
 
 # Set up argparse
 parser = argparse.ArgumentParser(description="Generate amino acid sequence combinations based on a CSV input file.")
 parser.add_argument('--input_file', type=str, required=True, help='CSV file containing position and amino acid list')
-parser.add_argument('--output_file', type=str, required=True, help='Output CSV file to save generated sequences')
 parser.add_argument('--length', type=int, default=13, help='Peptide length (default: 13)')
+parser.add_argument('--output_path', type=str, required=True, help='Directory path to save the result file')
 args = parser.parse_args()
+
+# Auto-generate output filename
+input_basename = os.path.splitext(os.path.basename(args.input_file))[0]
+output_filename = f"{input_basename}-shuffle.fasta"
+output_filepath = os.path.join(args.output_path, output_filename)
 
 # Read amino acid composition from CSV
 amino_acids = {}
-
 with open(args.input_file, newline='') as csvfile:
     reader = csv.DictReader(csvfile)
     for row in reader:
@@ -26,6 +31,6 @@ for combo in itertools.product(*[amino_acids[i] for i in range(1, args.length + 
     sequences.append(sequence)
 
 # Save sequences in FASTA format
-with open(args.output_file, 'w') as f:
+with open(output_filepath, 'w') as f:
     for i, sequence in enumerate(sequences):
         f.write(f'>seq{i}\n{sequence}\n')
